d0a50a9593137239d81c682c44cddfad084cae2c
Jan Steikowski <j.steikowski@aol.com>
2023-01-06 15:08:50 +0700

    Add 4x4 flex with responsive design

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let container = document.getElementById('container');
 
 function addDivs() {
     for (let i = 1; i <= 4;i++) {
+        for (j = 1; j <= 4;j++) {
         let div = document.createElement('div');
         container.appendChild(div);
-        div.className = 'divi';
+        div.className = 'div';
+        }
     }
 }
 
