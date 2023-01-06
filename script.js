beea4ee3a23b78760c57685fbdaa64ff345cf1e5
Jan Steikowski <j.steikowski@aol.com>
2023-01-06 18:19:53 +0700

    Add Eventlistener for hovering over the divs

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let container = document.getElementById('container');
 
-
 function addDivs() {
     for (let i = 1; i <= 16;i++) {
         for (j = 1; j <= 16;j++) {
         let div = document.createElement('div');
         container.appendChild(div);
         div.className = 'div';
+        div.addEventListener('mouseover',addHovered);
         }
     }
 }
+
+function addHovered() {
+    if (this.className === 'div') {
+        this.classList.add('divHovered');
+    }
+}
+
 
 addDivs()
