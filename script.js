d339b72476f7a538cbba7c2fd402c3db0769cf88
Jan Steikowski <j.steikowski@aol.com>
2023-01-06 15:25:46 +0700

    Extend 4x4 divFlex to a 16x16 divFlex

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let container = document.getElementById('container');
 
 
 function addDivs() {
-    for (let i = 1; i <= 4;i++) {
-        for (j = 1; j <= 4;j++) {
+    for (let i = 1; i <= 16;i++) {
+        for (j = 1; j <= 16;j++) {
         let div = document.createElement('div');
         container.appendChild(div);
         div.className = 'div';
