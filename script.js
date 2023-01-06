7d3b1eaea2233fa0251fe8f680aa7e6f7af3b1c1
Jan Steikowski <j.steikowski@aol.com>
2023-01-06 14:27:32 +0700

    Add function to add 4 divs to the html file inside container

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let container = document.getElementById('container');
 
-container.style.backgroundColor = 'black';
+
+function addDivs() {
+    for (let i = 1; i <= 4;i++) {
+        let div = document.createElement('div');
+        container.appendChild(div);
+        div.className = 'divi';
+    }
+}
+
+addDivs()
